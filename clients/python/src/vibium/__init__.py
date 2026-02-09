1920f65b42d1b04c8a669b7681f15d3c374440d5
Jason Huggins <hugs@vibium.com>
2026-02-09 05:54:32 -0600

    bump version to 0.1.7

--- clients/python/src/vibium/__init__.py
+++ clients/python/src/vibium/__init__.py
@@ -3,5 +3,5 @@
 from .browser import browser
 from .browser_sync import browser_sync
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 __all__ = ["browser", "browser_sync"]
--- packages/python/vibium_linux_arm64/src/vibium_linux_arm64/__init__.py
+++ packages/python/vibium_linux_arm64/src/vibium_linux_arm64/__init__.py
@@ -2,7 +2,7 @@
 
 from pathlib import Path
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 def get_binary_path() -> str:
     """Get the path to the clicker binary."""
